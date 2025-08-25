410949c1aacee2291164ce34568ce893dbb5b00c
Derrick Minor <codersyntaxdev@gmail.com>
2025-08-25 01:20:23 -0400

    release alpha v0.4

---
+++ js/main.js
@@ -0,0 +1,22 @@
+﻿window.PlayBackgroundMusic = (src, volume) => {
+    var audio = document.getElementById("background-music");
+    if (audio != null) {
+        var audioSource = document.getElementById("background-source-music");
+        if (audioSource != null) {
+            audioSource.src = window.location.href + src;
+            audio.load();
+            audio.volume = volume;
+            audio.play();
+        }
+    }
+}
+
+window.ChangeSoundVolume = (volume) => {
+    var audio = document.getElementById("background-music");
+    if (audio != null) {
+        var audioSource = document.getElementById("background-source-music");
+        if (audioSource != null) {
+            audio.volume = volume;
+        }
+    }
+}
